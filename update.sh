365a94318afcad003d1326a67ea01039561676e7
Migushthe2nd <ihaverkamp@live.nl>
2021-10-19 23:13:00 +0200

    Update scripts repo in update script

--- update.sh
+++ update.sh
@@ -2,6 +2,14 @@
 clear
 
 cd /var/lib/hydrominder/scripts/
+echo "##### Updating scripts..."
+# git reset hard and pull
+sudo git reset --hard && sudo git pull
+# chmod all scripts again
+sudo chmod ug+x /var/lib/hydrominder/scripts/*.sh > /dev/null 2>&1
+
+# TODO: restart shutdown-watcher.sh
+
 echo "##### Pulling container updates..."
 sudo docker-compose pull
 
