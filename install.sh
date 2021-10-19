591b27b805086d54c13f9e3ae59e1e578d532a23
Thomas van den Berg <thomasvdb2002@protonmail.com>
2021-10-19 16:41:42 +0200

    Initial docker-compose and install script improvements

--- install.sh
+++ install.sh
@@ -11,13 +11,13 @@ echo "##### Creating user that can't login, without home directory, add to 'hydr
 sudo useradd --system -M -g hydrominder hydrominder > /dev/null
 
 echo "##### Setting up directories..."
-sudo mkdir -p /var/lib/hydrominder/scripts > /dev/null
+sudo mkdir -p /var/lib/hydrominder > /dev/null
 sudo chown -R hydrominder:hydrominder /var/lib/hydrominder > /dev/null
 
 echo "##### Cloning the whole scripts repository..."
-cd /var/lib/hydrominder/scripts > /dev/null
+cd /var/lib/hydrominder/ > /dev/null
 # TODO: Overwrite existing directory
-sudo git clone https://gitlab.utwente.nl/cs21-32/hydrominderscripts.git
+sudo git clone https://gitlab.utwente.nl/cs21-32/hydrominderscripts.git scripts
 sudo chmod ug+x /var/lib/hydrominder/scripts/*.sh > /dev/null 2>&1
 
 echo "##### Installing Docker and required packages..."
@@ -33,17 +33,22 @@ echo "##### Creating self-signed SSL certificate..."
 # Valid for 10 years (TODO: automatically renew this)
 sudo -u hydrominder mkdir -p /var/lib/hydrominder/ssl > /dev/null
 
-sudo openssl req -newkey rsa:2048 -x509 -sha256 -days 3650 -nodes -subj "/C=US/ST=State/L=City/O=Dis/CN=*" -keyout /var/lib/hydrominder/ssl/hydrominder.key -out /var/lib/hydrominder/ssl/hydrominder.crt > /dev/null
+touch /var/lib/hydrominder/ssl/hydrominder.key
+touch /var/lib/hydrominder/ssl/hydrominder.crt
 echo "##### Making private key rw for user..."
 sudo chmod 600 /var/lib/hydrominder/ssl/hydrominder.key > /dev/null
 echo "##### Making public key rw for user, r for others..."
 sudo chmod 644 /var/lib/hydrominder/ssl/hydrominder.crt > /dev/null
+echo "##### Generating key..."
+sudo openssl req -newkey rsa:2048 -x509 -sha256 -days 3650 -nodes -subj "/C=US/ST=State/L=City/O=Dis/CN=*" -keyout /var/lib/hydrominder/ssl/hydrominder.key -out /var/lib/hydrominder/ssl/hydrominder.crt > /dev/null
 
 echo "##### Generating secure 64-byte key for API cookie..."
 COOKIE_SECRET=$(openssl rand -base64 32)
 
 echo "##### Generating password for DB..."
 DB_PASSWORD=$(openssl rand -base64 12)
+DB_HOST="postgres"
+export $DB_HOST
 
 echo "##### Generating key for Controller <-> API communication..."
 API_TOKEN=$(openssl rand -base64 32)
@@ -53,13 +58,21 @@ echo "##### Creating environment variable files..."
 sudo -u hydrominder mkdir -p /var/lib/hydrominder/var > /dev/null
 sudo chmod -R 600 /var/lib/hydrominder/var > /dev/null
 
+# DB env
+sudo -u hydrominder cat <<EOT >> /var/lib/hydrominder/var/db.env
+POSTGRES_DB=hydrominder
+POSTGRES_USER=hydrominder
+POSTGRES_PASSWORD=${DB_PASSWORD}
+EOT
+
 # API env
 sudo -u hydrominder cat <<EOT >> /var/lib/hydrominder/var/api.env
-DB_PASSWORD=${DB_PASSWORD}
+DB_HOST=postgres
+DB_PORT=5432
 SECRET=${COOKIE_SECRET}
 EOT
 
-# Web Appenv
+# Web App env
 sudo -u hydrominder cat <<EOT >> /var/lib/hydrominder/var/webapp.env
 EOT
 
@@ -77,12 +90,14 @@ sudo docker --config /var/lib/hydrominder/docker_configs/.hydrominder_api login 
 sudo docker --config /var/lib/hydrominder/docker_configs/.hydrominder_app login registry.gitlab.utwente.nl -u CLIENT -p SQyRA_B2hzBXZJ9sdiUs 2> /dev/null
 sudo docker --config /var/lib/hydrominder/docker_configs/.hydrominder_controller login registry.gitlab.utwente.nl -u CLIENT -p qV439CsvoBqdWKJ2z5-M 2> /dev/null
 
-echo "##### Pulling containers..."
+#echo "##### Pulling containers..."
 # API pull token: AZuNxneL16MhQ2xsxDBv (public)
-sudo docker --config /var/lib/hydrominder/docker_configs/.hydrominder_api pull registry.gitlab.utwente.nl/cs21-32/hydrominder_api > /dev/null
+#sudo docker --config /var/lib/hydrominder/docker_configs/.hydrominder_api pull registry.gitlab.utwente.nl/cs21-32/hydrominder_api > /dev/null
 # Web App pull token: SQyRA_B2hzBXZJ9sdiUs (public)
-sudo docker --config /var/lib/hydrominder/docker_configs/.hydrominder_app pull registry.gitlab.utwente.nl/cs21-32/hydrominder_app > /dev/null
+#sudo docker --config /var/lib/hydrominder/docker_configs/.hydrominder_app pull registry.gitlab.utwente.nl/cs21-32/hydrominder_app > /dev/null
 # Controller pull token: qV439CsvoBqdWKJ2z5-M (public)
-sudo docker --config /var/lib/hydrominder/docker_configs/.hydrominder_controller pull registry.gitlab.utwente.nl/cs21-32/hydrominder > /dev/null
+#sudo docker --config /var/lib/hydrominder/docker_configs/.hydrominder_controller pull registry.gitlab.utwente.nl/cs21-32/hydrominder > /dev/null
 
 # TODO: docker-compose up -d
+cd /var/lib/hydrominder/scripts/
+sudo docker-compose up -d
