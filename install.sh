6c1231f8124130b75d7701b87c4ab3b13ac5fb6a
Migushthe2nd <ihaverkamp@live.nl>
2021-10-19 16:52:35 +0200

    Use convenient docker install script

--- install.sh
+++ install.sh
@@ -22,9 +22,7 @@ sudo chmod ug+x /var/lib/hydrominder/scripts/*.sh > /dev/null 2>&1
 
 echo "##### Installing Docker and required packages..."
 sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release > /dev/null
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --yes --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "##### Adding GPG key..."
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+curl -fsSL https://get.docker.com | sudo sh -s
 echo "##### Installing docker pagackes..."
 sudo apt-get update > /dev/null
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose > /dev/null
