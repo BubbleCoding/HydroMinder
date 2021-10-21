9c5f89da066b37962db9d6a5290bbf060d2027a5
Migushthe2nd <ihaverkamp@live.nl>
2021-10-21 23:59:00 +0200

    Rename shutdown.txt, remove api port

--- shutdown-watcher.sh
+++ shutdown-watcher.sh
@@ -1,10 +1,10 @@
 # /bin/bash
 
 # This script will watch for changes in the shutdown file using inotify and will shutdown the machine if the contents are 'true'
-sudo echo "waiting" > /var/lib/hydrominder/shutdown.txt
-while inotifywait -e modify /var/lib/hydrominder/shutdown.txt; do
+sudo echo "waiting" > /var/lib/hydrominder/shutdown_signal
+while inotifywait -e modify /var/lib/hydrominder/shutdown_signal; do
     if [ -f /var/lib/hydrominder/shutdown.txt ]; then
-        if [ "$(cat /var/lib/hydrominder/shutdown.txt)" = "true" ]; then
+        if [ "$(cat /var/lib/hydrominder/shutdown_signal)" = "true" ]; then
             # shutdown the machine
             echo "Shutting down..."
             sudo shutdown -h now
