b344845c86a9749c1eaad80dd9b8c721aff7ce5f
Migushthe2nd <ihaverkamp@live.nl>
2021-11-01 11:10:28 +0100

    Fix files losing permissions before pull

--- update.sh
+++ update.sh
@@ -7,9 +7,11 @@ SCRIPTS_DIR="$INSTALL_DIR/scripts"
 cd $SCRIPTS_DIR
 echo "##### Updating scripts..."
 # git reset hard and pull
-sudo git update-index --refresh && sudo git reset --hard && sudo git pull
+sudo git reset --hard
 # chmod all scripts again
 sudo chmod ug+x $SCRIPTS_DIR/*.sh > /dev/null 2>&1
+# git pull
+sudo git pull
 
 # recreate signal-watcher.sh service
 sudo systemctl disable signal-watcher.service
