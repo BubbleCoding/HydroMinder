497f248988591728d06794c90eff67a108f4a679
Migushthe2nd <ihaverkamp@live.nl>
2021-11-01 12:01:18 +0100

    Remove target, use plain service files

--- signal-watchers/create.sh
+++ signal-watchers/create.sh
@@ -1,9 +1,7 @@
 #! /bin/bash
 
-sudo systemctl stop signal-watcher.target
 sudo systemctl disable $SCRIPTS_DIR/signal-watchers/shutdown-watcher.service
 sudo systemctl disable $SCRIPTS_DIR/signal-watchers/update-watcher.service
 
 sudo systemctl enable $SCRIPTS_DIR/signal-watchers/shutdown-watcher.service
 sudo systemctl enable $SCRIPTS_DIR/signal-watchers/update-watcher.service
-sudo systemctl start signal-watcher.target
