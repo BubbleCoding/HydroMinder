7f3df34184d10cb19d34ddf105b785049f3aec56
Migushthe2nd <ihaverkamp@live.nl>
2021-11-01 12:04:50 +0100

    Provide scripts path in create.sh

--- signal-watchers/create.sh
+++ signal-watchers/create.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
+INSTALL_DIR="/var/lib/hydrominder"
+SCRIPTS_DIR="$INSTALL_DIR/scripts"
 
 sudo systemctl disable $SCRIPTS_DIR/signal-watchers/shutdown-watcher.service
 sudo systemctl disable $SCRIPTS_DIR/signal-watchers/update-watcher.service
