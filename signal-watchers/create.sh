9ee2bd9c142bc050f9c3306cc589c7a0555dfe14
Migushthe2nd <ihaverkamp@live.nl>
2021-11-01 12:08:31 +0100

    Disable services by name

--- signal-watchers/create.sh
+++ signal-watchers/create.sh
@@ -2,8 +2,8 @@
 INSTALL_DIR="/var/lib/hydrominder"
 SCRIPTS_DIR="$INSTALL_DIR/scripts"
 
-sudo systemctl disable $SCRIPTS_DIR/signal-watchers/shutdown-watcher.service
-sudo systemctl disable $SCRIPTS_DIR/signal-watchers/update-watcher.service
+sudo systemctl disable shutdown-watcher.service
+sudo systemctl disable update-watcher.service
 
 sudo systemctl enable $SCRIPTS_DIR/signal-watchers/shutdown-watcher.service
 sudo systemctl enable $SCRIPTS_DIR/signal-watchers/update-watcher.service
