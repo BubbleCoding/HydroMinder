a04a9909b58f6df2dbaae2cc4969e1cecccbbbbb
Migushthe2nd <ihaverkamp@live.nl>
2021-11-01 11:05:25 +0100

    Ignore execution permissions in git reset

--- update.sh
+++ update.sh
@@ -7,7 +7,7 @@ SCRIPTS_DIR="$INSTALL_DIR/scripts"
 cd $SCRIPTS_DIR
 echo "##### Updating scripts..."
 # git reset hard and pull
-sudo git reset --hard && sudo git pull
+sudo git update-index --refresh && sudo git reset --hard && sudo git pull
 # chmod all scripts again
 sudo chmod ug+x $SCRIPTS_DIR/*.sh > /dev/null 2>&1
 
