93e1edbb38bcb4f9ccf354f7d734d841333434f1
s2532743 <t.vandenberg-5@student.utwente.nl>
2021-11-02 16:20:58 +0000

    Actually added the GPG-key. Previously only the repository was added, without the key for it.

--- install.sh
+++ install.sh
@@ -31,7 +31,8 @@ find $SCRIPTS_DIR -type f -name "*.sh" | xargs sudo chmod ug+x
 
 echo "##### Installing Docker and required packages..."
 sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release > /dev/null
-echo "##### Adding GPG key..."
+echo "##### Adding repository..."
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg > /dev/null
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 echo "##### Installing docker packages..."
 sudo apt-get update > /dev/null
