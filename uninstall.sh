ca169dd222044f277cf4328356ac0ec843a6c3c6
s2532743 <t.vandenberg-5@student.utwente.nl>
2021-11-03 12:25:58 +0000

    Added uninstall.sh Added a small uninstall script.

---
+++ uninstall.sh
@@ -0,0 +1,26 @@
+#! /bin/sh
+echo "##### Uninstalling hydrominder"
+read -r -p "Are You Sure? [y/N] " input
+ 
+case $input in
+    [yY][eE][sS]|[yY])
+    echo "##### Stopping hydrominder..."
+    cd /var/lib/hydrominder/scripts && sudo docker-compose stop
+    echo "##### Removing docker containers, volumes, etc..."
+    sudo docker system prune -af > /dev/null
+    sudo docker volume prune -f > /dev/null
+    echo "##### Removing files..."
+    cd /var/lib
+    sudo rm -rf /var/lib/hydrominder
+    sudo rm -f /usr/share/keyrings/docker-archive-keyring.gpg
+    echo "##### Removing user..."
+    sudo userdel hydrominder > /dev/null
+ ;;
+    [nN][oO]|[nN]|'')
+ echo "##### Cancelling..."
+       ;;
+    *)
+ echo "##### Invalid input..."
+ exit 1
+ ;;
+esac
