25856bc610c21719d0e19679f6526f80cc6bd32f
Migushthe2nd <ihaverkamp@live.nl>
2021-11-05 21:39:56 +0100

    Don't stop service to prevent stopping itself

--- signal-watchers/create.sh
+++ signal-watchers/create.sh
@@ -5,12 +5,9 @@ SCRIPTS_DIR="$INSTALL_DIR/scripts"
 sudo -u hydrominder touch $INSTALL_DIR/shutdown_signal
 sudo -u hydrominder touch $INSTALL_DIR/update_signal
 
-sudo systemctl stop shutdown-watcher.service
-sudo systemctl disable shutdown-watcher.service
-sudo systemctl stop update-watcher.service
-sudo systemctl disable update-watcher.service
-
 sudo systemctl enable $SCRIPTS_DIR/signal-watchers/shutdown-watcher.service
 sudo systemctl start shutdown-watcher.service
 sudo systemctl enable $SCRIPTS_DIR/signal-watchers/update-watcher.service
 sudo systemctl start update-watcher.service
+
+sudo systemctl daemon-reload
