d16363b9def014f21bbce3165238a6c943c04f5e
Migushthe2nd <ihaverkamp@live.nl>
2021-11-05 18:26:36 +0100

    Fix incorrect controller name

--- pull-containers.sh
+++ pull-containers.sh
@@ -13,7 +13,7 @@ sudo docker --config $INSTALL_DIR/docker_configs/.hydrominder_controller pull re
 
 DOCKER_DIGEST_hydrominder_api=$(sudo docker images -q --no-trunc registry.gitlab.utwente.nl/cs21-32/hydrominder_api:latest | sed 's/sha256://g')
 DOCKER_DIGEST_hydrominder_app=$(sudo docker images -q --no-trunc registry.gitlab.utwente.nl/cs21-32/hydrominder_app:latest | sed 's/sha256://g')
-DOCKER_DIGEST_hydrominder=$(sudo docker images -q --no-trunc registry.gitlab.utwente.nl/cs21-32/hydrominder_controller:latest | sed 's/sha256://g')
+DOCKER_DIGEST_hydrominder=$(sudo docker images -q --no-trunc registry.gitlab.utwente.nl/cs21-32/hydrominder:latest | sed 's/sha256://g')
 # Docker digests env
 sudo -u hydrominder touch $VAR_DIR/docker_digests.env
 sudo -u hydrominder tee $VAR_DIR/docker_digests.env <<EOT > /dev/null
