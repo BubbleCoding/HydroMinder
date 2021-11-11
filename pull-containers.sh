b0256ae62af481ee57fd9c63e0986def264af5df
Migushthe2nd <ihaverkamp@live.nl>
2021-11-11 09:34:26 +0100

    Switch to 32bit container

--- pull-containers.sh
+++ pull-containers.sh
@@ -9,11 +9,11 @@ sudo docker --config $INSTALL_DIR/docker_configs/.hydrominder_api pull registry.
 # Web App pull
 sudo docker --config $INSTALL_DIR/docker_configs/.hydrominder_app pull registry.gitlab.utwente.nl/cs21-32/hydrominder_app:latest > /dev/null
 # Controller pull 
-sudo docker --config $INSTALL_DIR/docker_configs/.hydrominder_controller pull registry.gitlab.utwente.nl/cs21-32/hydrominder:latest > /dev/null
+sudo docker --config $INSTALL_DIR/docker_configs/.hydrominder_controller pull registry.gitlab.utwente.nl/cs21-32/hydrominder:32bit > /dev/null
 
 DOCKER_DIGEST_hydrominder_api=$(sudo docker images -q --no-trunc registry.gitlab.utwente.nl/cs21-32/hydrominder_api:latest | sed 's/sha256://g')
 DOCKER_DIGEST_hydrominder_app=$(sudo docker images -q --no-trunc registry.gitlab.utwente.nl/cs21-32/hydrominder_app:latest | sed 's/sha256://g')
-DOCKER_DIGEST_hydrominder=$(sudo docker images -q --no-trunc registry.gitlab.utwente.nl/cs21-32/hydrominder:latest | sed 's/sha256://g')
+DOCKER_DIGEST_hydrominder=$(sudo docker images -q --no-trunc registry.gitlab.utwente.nl/cs21-32/hydrominder:32bit | sed 's/sha256://g')
 # Docker digests env
 sudo -u hydrominder touch $VAR_DIR/docker_digests.env
 sudo -u hydrominder tee $VAR_DIR/docker_digests.env <<EOT > /dev/null
